326a711c7828df20aef5e7e3da2b1e21176a8682
Darryl <darrylmangibin@gmail.com>
2019-02-12 16:14:12 +0800

    install react modules and config webpack to run react

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('app.js is running');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <p>test</p>;
+
+ReactDOM.render(template, document.querySelector('#app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-// entry
+
 const path = require('path');
 
 module.exports = {
@@ -6,5 +6,13 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
     }
 }
+
